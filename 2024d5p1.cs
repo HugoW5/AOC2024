d869d87f0fba87ea8f42c701cb8910c721d6fe10
Hugo Wakt <32434494+HugoW5@users.noreply.github.com>
2024-12-05 10:35:43 +0100

    Update 2024d5p1.cs

--- 2024d5p1.cs
+++ 2024d5p1.cs
@@ -6,45 +6,70 @@ using System.Threading.Tasks;
 
 namespace AOC2024
 {
-	internal class _2024d5p1
-	{
-		public static void Run()
-		{
-			string input = @"47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
+ internal class _2024d5p1
+ {
+     public static void Run()
+     {
+         string input = File.ReadAllText("input.txt");
 
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47";
-			string[] blocks = input.Split(new string[] { "\n\n", "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+         string[] blocks = input.Split(new string[] { "\n\n", "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+         var tmpRules = blocks[0].Split('\n');
+         var tmpUpdates = blocks[1].Split('\n');
 
-			// Output each block
-			for (int i = 0; i < blocks.Length; i++)
-			{
-				Console.WriteLine($"Block {i + 1}:\n{blocks[i].Trim()}\n");
-			}
-		}
-	}
+         List<List<int>> updates = new();
+         List<(int, int)> rules = new();
+
+         int total = 0;
+
+         foreach (var rule in tmpRules)
+         {
+             var tmpr = rule.Split('|');
+             rules.Add((int.Parse(tmpr[0]), int.Parse(tmpr[1])));
+         }
+         foreach (var line in tmpUpdates)
+         {
+             var updateLine = line.Split(',').Select(int.Parse).ToList();
+             updates.Add(updateLine);
+         }
+
+         foreach (var update in updates)
+         {
+             if (IsValidUpdate(update, rules))
+             {
+                 total += middleNum(update);
+             }
+         }
+
+         Console.WriteLine(total);
+
+     }
+
+     private static int middleNum(List<int> updates)
+     {
+         return updates[updates.Count / 2];
+     }
+
+     private static bool IsValidUpdate(List<int> update, List<(int X, int Y)> rules)
+     {
+
+         Dictionary<int, int> postions = new Dictionary<int, int>();
+         for (int i = 0; i < update.Count; i++)
+         {
+             postions.Add(update[i], i);
+         }
+
+         foreach (var rule in rules)
+         {
+             if (postions.ContainsKey(rule.X) && postions.ContainsKey(rule.Y))
+             {
+                 if (postions[rule.X] > postions[rule.Y])
+                 {
+                     return false;
+                 }
+             }
+             Console.WriteLine(rule);
+         }
+         return true;
+     }
+ }
 }
